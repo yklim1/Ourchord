270af086814a2639dad73775e0979037c93393e0
flottante301 <55368033+flottante301@users.noreply.github.com>
2020-02-27 17:45:01 +0900

    Update Transparency_score.py

--- min/Transparency_score.py
+++ min/Transparency_score.py
@@ -1,5 +1,22 @@
+#import numpy as np
+#import cv2
+#from wand.image import Image
+#IMREAD_COLOR(1): color로 읽기
+#IMREAD_GRAY_SCALE(0): GRAYSCALE로 읽기
+#IMREAD_UNCHAGED(-1): ALPHA CHANNEL까지 포함하여 읽기
+
+# only black img save success
+
+#blackimg = Image(filename="qutest-0.png", resolution=300)
+#with Image(filename='qutest-0.png') as img:
+
+    #img.save(filename='suc.png')
+#
+#Transparency_score.py
 from PIL import Image
 
+#i=1
+#img=Image.open('3test-'+i+'.png')
 '''
 for i in range(76,100): #10_076~100
     #print(f'10_07{i}.PNG')
@@ -11,14 +28,17 @@ for i in range(0,5):
 '''#endtest score success(wartermark x)
 
 #3.pdf test
-for i in range(0,6):
-    img=Image.open(f'3-{i}.PNG')
+#for i in range(0,6):
+#    img=Image.open(f'3-{i}.PNG')
 
+for i in range(1,9):
+    #img = Image.open(f'3-{i}.PNG')
+    img = Image.open(f'C://cnntrain//Note//Template Match//hote2//rest2_0{i}.PNG')
 #img=img.convert("RGBA")
     datas=img.getdata()
 
     newData = []
-    Cutoff=150
+    Cutoff=50 #10이하는 비슷한걸로 보임 10이랑 100은 차이남
 
     for item in datas:
         if item[0] >= Cutoff and item[1] >=Cutoff and item[2] >=Cutoff:
@@ -29,9 +49,11 @@ for i in range(0,6):
 
     img.putdata(newData)
     # 3.pdf test
-    img.save(f'new3-{i}.PNG')
+    #img.save(f'new3-{i}.PNG')
+    img.save(f'C://cnntrain//Note//Template Match//hote2//rest2_0{i}.PNG')
+    #img.save(f'3-{i}.PNG', "PNG")
 '''
-for i in range(76, 100):
+for i in range(76, 99):
     img.save(f'new10_0{i}.PNG',"PNG")
 ''' #rest success
 '''
