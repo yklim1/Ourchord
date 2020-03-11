afbe080b7c8d06d3ca89779f992f2e4534ecdfbc
flottante301 <55368033+flottante301@users.noreply.github.com>
2020-03-11 15:29:23 +0900

    Update speaker.py

--- 01_SPEAKER/speaker.py
+++ 01_SPEAKER/speaker.py
@@ -15,11 +15,10 @@ while 1: #user input only A to G
     for i in tuning: #i=='C'~'B'
         #print(i)
         ex +=1
-        #'C' to 'B'까지 순차적으로 접근해서 조건 만족 시 출력 
         if tuning_sound==i: #tuning_sound=='C'
             print("Start "+tuning_sound+" play")
             os.system(f"start C:/{tuning_sound}.mp3") #file name: only english
-            sys.exit(1) #출력 후 종료
+            sys.exit(1)
 
     if ex==7: #if input data is not A to G
         print("please input A to G !!")
