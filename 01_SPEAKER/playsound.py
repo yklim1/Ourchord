5fde8121d5856b1671b68a84db2d9c30fbba0cc5
flottante301 <55368033+flottante301@users.noreply.github.com>
2020-03-11 06:27:37 +0000

    Delete playsound.py

--- 01_SPEAKER/playsound.py
+++
@@ -1,7 +0,0 @@
-import os
-
-tuning_sound=input("what did you want?(A ~ G): ") 
-
-print("Start "+tuning_sound+" play")
-
-os.system(f"start C:\{tuning_sound}.mp3") #file name: only english/korean:error occur
