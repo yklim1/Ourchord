cc682057c8e2c62f43e7b0aab1d3badee996d08d
flottante301 <55368033+flottante301@users.noreply.github.com>
2020-03-12 16:12:27 +0900

    20/03/12

--- 02_PDF2PNG/pdftopng.py
+++ 02_PDF2PNG/pdftopng.py
@@ -1,4 +1,4 @@
 from wand.image import Image
 with Image(filename='endtest.pdf',resolution=200) as img: #image resolution(200/500)
     img.format="png"
-    img.save(filename='endtest.png')#endtest.0~ Auto rename
+    img.save(filename='endtest.png')
