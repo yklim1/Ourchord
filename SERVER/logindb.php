939ce53281a80fad4d902bfca3e2c80d84938bcc
flottante301 <55368033+flottante301@users.noreply.github.com>
2020-03-12 16:02:25 +0900

    Update logindb.php

--- SERVER/logindb.php
+++ SERVER/logindb.php
@@ -2,7 +2,7 @@
     <html lang="ko">
         <head>
         <meta charset="utf-8">
-        <title>Login Test</title>
+        <title>LoginDB Test</title>
         <style>
             body {
                 font-family: Consolas, monospace;
