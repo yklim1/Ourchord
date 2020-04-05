5b90e3b75f332a6d4074eb391501e7be862f234a
yklim1 <57138149+yklim1@users.noreply.github.com>
2020-04-06 08:47:05 +0900

    Update staff_gap_resize_v1.py

--- 03_DETECT_STAFF/staff_gap_resize_v1.py
+++ 03_DETECT_STAFF/staff_gap_resize_v1.py
@@ -9,6 +9,7 @@ cv2.waitKey(0)
 
 
 # 2배 이미지
+# 밑의 fx와 fy에 이미지 비율에 대한 값이 들어가야 함. 값은 구하는 값은 다른 곳에 구현 되어 있다.
 img_result = cv2.resize(img_source, None, fx=1.625, fy=1.625, interpolation = cv2.INTER_CUBIC)
 cv2.imshow("x2", img_result)
 cv2.imwrite(".vscode//resizeaa.png",img_result)
