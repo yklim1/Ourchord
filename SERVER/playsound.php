3e29955aac5bf0b79a2ef219839f18b60faa8fa5
flottante301 <55368033+flottante301@users.noreply.github.com>
2020-04-09 19:01:26 +0900

    Update playsound.php

--- SERVER/playsound.php
+++ SERVER/playsound.php
@@ -2,16 +2,16 @@
 header("Content-Type: text/html; charset=UTF-8");
 
 phpinfo();
-//안드로이드에서 튜닝음 받아오기
+//안스 튜닝음 받아오기
 $tuning=$_POST['tuning'];
 
-//아무것도 입력받지 않았을 때(cf. radio button)
+//exc(cf. radio button)
 if(empty($tuning)){
     //$errMSG="듣고 싶은 튜닝음을 선택해주세요.";
     echo "듣고 싶은 튜닝음을 선택해주세요.<br><br>";
 }
 
-//정상작동
+//suc
 //if(!isset($errMSG)){
 else{
       //1.DB접근
@@ -29,8 +29,7 @@ else{
           }
 
       //2.SQL에서 튜닝음 경로 추출
-      //SELECT(찾기) INSERT(삽입) UPDATE(수정) DELETE(삭제)
-      //Sound db table에서 tuning이 $tuning값에 해당되는 값을 가져와라
+     
       $sql= "SELECT *FROM Sound where tuning='$tuning'";
       $result=mysqli_query($con, $sql);
       while($row=mysqli_fetch_array($result)){
