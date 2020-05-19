87c534c004ed7a876ac96ea56a2176bccd18319c
flottante301 <55368033+flottante301@users.noreply.github.com>
2020-05-20 03:28:32 +0900

    Update p_serv_db.py

--- SERVER/DB/p_serv_db.py
+++ SERVER/DB/p_serv_db.py
@@ -1,14 +1,21 @@
+#python에서 rds접근 test용
 import pymysql
 
+#mysql연동
 connect = pymysql.connect(host="localhost",
                           port=3306,
                           user="사용자이름",
                           password="비번",
-                          db="DB이")
+                          db="DB이름")
 
+#aws rds에서 커서 cursor얻기
+#cursor: DB에서 sql문장 수행역할하기 위함
 cursor = connect.cursor()
 
+#USER테이블 전체 조회
 cursor.execute("SELECT *FROM USER")
+
+#result에 저장(데이터 패치)
 result = cursor.fetchall()
 
 print(result)
